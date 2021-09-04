487f1f9098d37e1eedd17374ac1ca670682eed32
Jhony <jhony.v.m@hotmail.com>
2021-09-04 16:55:12 -0300

    Criando conexao com Docker MYSQL

--- src/main/java/jhony/villani/springboot2essentials/domain/Filme.java
+++ src/main/java/jhony/villani/springboot2essentials/domain/Filme.java
@@ -2,10 +2,20 @@ package jhony.villani.springboot2essentials.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data //gera automaticamente getters/setters/toString
 @AllArgsConstructor //inicia construtor com todas as variáveis
+@NoArgsConstructor
+@Entity
 public class Filme {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
